055b48b5de7be664b9cbcbeb2c7f962d83a45616
PrAndrian <princydruh@gmail.com>
2023-03-03 19:24:11 +0100

    line-breack stop fix

--- scripts/factories/recipes.js
+++ scripts/factories/recipes.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const recipeFactory = (data) => {
   const {
     name,
--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-plusplus */
 function isLowerCaseContains(text, word) {
   const lenText = text.length;
